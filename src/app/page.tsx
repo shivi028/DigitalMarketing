856f00d54fbc6d4beb0607ba863cbaafa929b77b
shivi028 <tiwarishivi028@gmail.com>
2025-02-24 10:43:07 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import LandingPage from "@/components/LandingPage/LandingPage";
 import WhatWeDo from "@/components/WhatWeDo/WhatWeDo";
-import Image from "next/image";
 
 export default function Home() {
   return (
